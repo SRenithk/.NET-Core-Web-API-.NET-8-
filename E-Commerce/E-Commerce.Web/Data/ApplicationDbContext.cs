50ce1859fb7ffb9890c0b28d62ae744717179b10
unknown <renithKumar.senthil@hexacorp.com>
2023-11-22 12:51:23 +0530

    Created New project Web API

---
+++ E-Commerce/E-Commerce.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ E-Commerce/E-Commerce.Web/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Commerce.Web.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
